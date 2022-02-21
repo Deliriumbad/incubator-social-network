ac7594a594184b77a3ffe0d03930f7e03fcd565b
Vit <vit_waladzko@mail.ru>
2022-02-22 00:17:13 +0300

    '30lesson/1'

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,57 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
-import {Header} from "./components/Header/Header";
-import {Navbar} from "./components/Navbar/Navbar";
-import {Profile} from "./components/Profile/Profile";
-import {Dialogs} from "./components/Dialogs/Dialogs";
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
-import {News} from "./components/News/News";
-import {Settings} from "./components/Settings/Settings";
-import {Music} from "./components/Music/Music";
+import {TaskPropsType, Todolist} from "./Todolist";
+import {v1} from "uuid";
 
+export type FilterValuesType = 'all' | 'completed' | 'active';
 
+function App() {
 
+ let [tasks, setTasks] = useState<Array<TaskPropsType>>([
+     { id: v1(), title: "HTML&CSS", isDone: true },
+     { id: v1(), title: "JS", isDone: true },
+     { id: v1(), title: "ReactJS", isDone: false },
+     { id: v1(), title: "Redux", isDone: true }
+ ])
 
+    let[filter,setFilter] = useState<FilterValuesType>('all') //active
 
-const App = () => {
+    console.log('---> tasks',tasks);
+    let tasksForTodolist = tasks; //4
+
+    if(filter==='completed'){
+        tasksForTodolist = tasks.filter(el => el.isDone === true);
+    }
+    if(filter==='active'){
+        tasksForTodolist = tasks.filter(el => el.isDone === false);//1
+    }
+    console.log('---> tasksForTodolist',tasksForTodolist);
+    function changeFilter (value:FilterValuesType){
+        setFilter(value);
+    }
+
+    function removeTask (newId:string){
+        let filteredTask = tasks.filter(el => el.id !== newId);
+        setTasks(filteredTask);
+    }
+
+    function addTask (title:string) {
+        let newTask = { id: v1(), title: title, isDone: true };
+        let newTasks = [newTask, ...tasks];
+        setTasks(newTasks);
+    }
+
+    console.log('render');
     return (
-        <BrowserRouter>
-            <div className='app-wrapper'>
-                <Header/>
-                <Navbar/>
-                <div className='app-wrapper-content'>
-                    <Routes>
-                        <Route path='/profile/*' element={<Profile/>}/>
-                        <Route path='/dialogs/*' element={<Dialogs/>}/>
-                        <Route path='/news/*' element={<News/>}/>
-                        <Route path='/music/*' element={<Music/>}/>
-                        <Route path='/settings/*' element={<Settings/>}/>
-                    </Routes>
-                </div>
-            </div>
-        </BrowserRouter>
+        <div className="App">
+            <Todolist
+                title='What to learn'
+                task={tasksForTodolist}//1
+                removeTask={removeTask}
+                changeFilter={changeFilter}
+                addTask={addTask}
+            />
+        </div>
     );
 }
 
